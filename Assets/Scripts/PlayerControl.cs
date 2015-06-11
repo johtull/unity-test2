224abd04f97a19b93aad01da75fffca899763664
johtull <robert.brestle@gmail.com>
2015-06-10 21:27:33 -0500

    Toasty Setup
    MOVE

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class PlayerControl : MonoBehaviour {
 	public float speed = 6.0F;
-	public float jumpSpeed = 80.0F;
+	public float jumpSpeed = 20.0F;
 	public float gravity = 20.0F;
 	private Vector3 moveDirection = Vector3.zero;
 	void Update() {
