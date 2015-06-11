cf38d10561a54c9eb8b193dd3eb623c319544aac
GDLtamond <alexf.side@gmail.com>
2015-06-10 23:36:49 -0500

    Double jump

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,6 +5,10 @@ public class PlayerControl : MonoBehaviour {
 	public float speed = 6.0F;
 	public float jumpSpeed = 20.0F;
 	public float gravity = 20.0F;
+
+	public bool jumping = false;
+	public bool jumping2 = false;
+
 	private Vector3 moveDirection = Vector3.zero;
 	void Update() {
 		CharacterController controller = GetComponent<CharacterController>();
@@ -12,8 +16,22 @@ public class PlayerControl : MonoBehaviour {
 			moveDirection = new Vector3(Input.GetAxis("NewHor"), 0, Input.GetAxis("NewVert"));
 			moveDirection = transform.TransformDirection(moveDirection);
 			moveDirection *= speed;
-			if (Input.GetButton("NewJump"))
+			if (Input.GetButtonDown("NewJump"))
 				moveDirection.y = jumpSpeed;
+			jumping2 = false;
+		}
+
+		else{
+			
+			if (Input.GetButtonDown("NewJump") && !jumping2)
+			{
+				moveDirection = new Vector3(Input.GetAxis("NewHor"), 0, Input.GetAxis("NewVert"));
+				moveDirection = transform.TransformDirection(moveDirection);
+				moveDirection *= speed;
+				moveDirection.y = jumpSpeed;
+				jumping2 = true;
+				print ("VDVD");
+			}
 			
 		}
 		moveDirection.y -= gravity * Time.deltaTime;
