2a1b9cf065c13dc11785513c3699da45150daec4
johtull <robert.brestle@gmail.com>
2015-06-14 21:22:48 -0500

    Skill class
    Basic functionality for player skills.

---
+++ Assets/Scripts/Skill.cs
@@ -0,0 +1,68 @@
+﻿using System.IO;
+using UnityEngine;
+using System.Collections;
+
+
+
+public class Skill : MonoBehaviour {
+
+	private float experience;
+	private string skillName;
+	private Hashtable abilities;
+
+
+
+	public Skill(string n) {
+		skillName = n;
+		experience = 0;
+		abilities = loadSkill(skillName);
+		//UnityEngine.Debug.Log("level: " + getLevel ());
+	}
+
+	public Skill(float xp, string n) {
+		experience = xp;
+		skillName = n;
+		abilities = loadSkill(skillName);
+	}
+
+
+	/*void Start () {
+		//UnityEngine.Debug.Log("info");
+		Skill mySkill = new Skill("fishing");
+	}*/
+
+	public float Experience {
+		get {
+			return experience;
+		}
+	}
+
+	public void addXP(float xp) {
+		experience += xp;
+	}
+	public void removeXP(float xp) {
+		experience -= xp;
+	}
+
+	// Experience = Level^3 * 10
+	// Level = experience^(1/3) / 10^(1/3)
+	public int getLevel() {
+		return (int)((float)System.Math.Pow(experience, (1.0)/(3.0)) / (float)System.Math.Pow(10, (1.0)/(3.0)));
+	}
+
+	// load skills from flat file
+	private Hashtable loadSkill(string file) {
+		Hashtable myAbilities = new Hashtable ();
+		using (var sr = File.OpenText("Assets/Config/" + file + ".csv")) {
+			string line;
+			while ((line = sr.ReadLine()) != null)
+			{
+				var fields = line.Split(',');
+				myAbilities.Add(fields[0].Trim(),fields[1].Trim());
+			}
+		}
+		return myAbilities;
+	}
+
+}
+
