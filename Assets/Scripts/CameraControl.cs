c73d341b55eb6e5376b431bdb6014f348adbb66e
GDLtamond <alexf.side@gmail.com>
2015-06-15 22:28:58 -0500

    Camera Changes

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class CameraControl : MonoBehaviour {
 
+	public Camera mainCam;
 	public GameObject targetObject;
 	public GameObject targetLook;
 	public Vector3 targetPosition;
@@ -20,6 +21,9 @@ public class CameraControl : MonoBehaviour {
 	public float ty;
 	public float tz;
 
+	public float mouseX;
+	public float mouseY;
+
 	//public int gamemode = gameMode;
 	public bool focus = false;
 	public bool rotate = false;
@@ -34,6 +38,9 @@ public class CameraControl : MonoBehaviour {
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		RaycastHit hit;
 
+		//mouseX=0;
+		//mouseY=0;
+
 		if(Input.GetKeyDown ("c") && !pan)
 		{
 		//	targetLook = null;
@@ -46,6 +53,11 @@ public class CameraControl : MonoBehaviour {
 			rotate = true;
 		else if(Input.GetKeyDown ("v") && rotate)
 			rotate = false;
+
+		if(Input.GetMouseButton(1))
+		   rotate = true;
+		else
+		   rotate = false;
 		
 		//transform.position = new Vector3(targetObject.transform.position.x - 10, targetObject.transform.position.y + 10, targetObject.transform.position.z - 10);
 		//transform.LookAt(targetLook.transform.position);
@@ -74,7 +86,9 @@ public class CameraControl : MonoBehaviour {
 			tz = targetObject.transform.position.z + dz;
 			transform.position = new Vector3(tx, ty, tz);
 			targetLook = targetObject;
-			transform.LookAt(targetLook.transform.position);
+			mainCam.gameObject.transform.LookAt(targetLook.transform.position);
+
+
 		}
 		else
 		{
@@ -86,20 +100,28 @@ public class CameraControl : MonoBehaviour {
 			ty = targetObject.transform.position.y + dy;
 			tz = targetObject.transform.position.z + dz;
 			transform.position = new Vector3(tx, ty, tz);
+
+
 			//transform.LookAt(targetLook.transform.position);
 		}
+		//mouseMove ();
+		//mouseX = Input.mousePosition.x;
+		//mouseY = Input.mousePosition.y;
+
 
 		//Rotate Options
 		if(rotate)
 		{
-			if(Input.GetKey("up"))
+			/*if(Input.GetKey("up"))
 				transform.Rotate(1,0,0);
 			if(Input.GetKey("down"))
 				transform.Rotate(-1,0,0);
 			if(Input.GetKey("right"))
 				transform.Rotate(0,1,0);
 			if(Input.GetKey("left"))
-				transform.Rotate(0,-1,0);
+				transform.Rotate(0,-1,0);*/
+
+			mouseMove ();
 		}
 		else
 		{
@@ -112,6 +134,9 @@ public class CameraControl : MonoBehaviour {
 			if(Input.GetKey("left"))
 				theta +=1f;
 		}
+		mouseX = Input.mousePosition.x;
+		mouseY = Input.mousePosition.y;
+		
 
 		//Click Something to refocus
 		/*
@@ -166,4 +191,30 @@ public class CameraControl : MonoBehaviour {
 		}*/
 
 	}
+
+	public void mouseMove()
+	{
+		float easeFactor = 10f;
+		if(Input.GetMouseButton(1))
+		{
+			//Horizontal
+			if(Input.mousePosition.x != mouseX)
+			{
+				float rotY = (Input.mousePosition.x - mouseX) * easeFactor * Time.deltaTime;
+				this.transform.Rotate(0, rotY, 0);
+			}
+
+			//Vertical
+			if(Input.mousePosition.y != mouseY)
+			{
+
+				float rotX = (mouseY - Input.mousePosition.y) * easeFactor * Time.deltaTime;
+
+				mainCam.gameObject.transform.Rotate(rotX, 0, 0);
+			}
+
+		}
+		
+	}
+
 }
