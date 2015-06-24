f48d0b1b30101244e2930eee6979f1a7409b4f57
GDLtamond <alexf.side@gmail.com>
2015-06-23 19:55:45 -0500

    Ron Paul

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+	public void OnBeginDrag(PointerEventData eventData){
+
+	}
+
+	public void OnDrag(PointerEventData eventData){
+		this.transform.position = eventData.position;
+	}
+
+	public void OnEndDrag(PointerEventData eventData){
+		
+	}
+}
